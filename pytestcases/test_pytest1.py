4e11f306f8376f3e4ec640e550c721566d64a8e0
nmm1108 <1290748875@qq.com>
2020-11-03 16:51:42 +0800

    commit readme

--- pytestcases/test_pytest1.py
+++ pytestcases/test_pytest1.py
@@ -1,4 +1,6 @@
 import pytest
+
+
 class TestDemo():
     def test_one(self):
         print("开始执行 test_one 方法")
@@ -12,23 +14,23 @@ class TestDemo():
         print("开始执行 test_three 方法")
         # x = 'this'
         # assert 'h' in x
-class  TestDemo1():
+
+
+class TestDemo1():
     def test_a(self):
         print("开始执行 test_a 方法")
         x = 'this'
         assert 'h' in x
+
     def test_b(self):
         print("开始执行 test_b 方法")
 
-
     def test_c(self):
         print("开始执行 test_c 方法")
-        #x = 'this'
-        #assert 'h' in x
+        # x = 'this'
+        # assert 'h' in x
+
 
 if __name__ == '__main__':
-    #pytest.main("-v -x TestDemo")
-    pytest.main(['-v','-s','TestDemo'])
-
-
-
+    # pytest.main("-v -x TestDemo")
+    pytest.main(['-v', '-s', 'TestDemo'])
